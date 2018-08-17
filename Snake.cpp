973431ba70e5b8bfc234a7838ef890393ef30875
contrbas@gmail.com <K4rmU78B>
2018-08-17 15:44:04 +0300

    ALL

--- Snake.cpp
+++ Snake.cpp
@@ -5,32 +5,11 @@
 Snake::Snake(){}
 Snake::~Snake() {}
 
-// std::vector<std::pair<int, int>> cor;
-
-// cor.push_back(std::make_pair(1, 1));
-// cor.push_back(std::make_pair(2, 2));
-// cor.push_back(std::make_pair(3, 3));
-
-// for (size_t i = 0; i < cor.size() - 1; i++)
-// {
-//     std::swap(cor[i], cor[i + 1]);
-// }
-
-// for (size_t i = 0; i < cor.size(); i++)
-// {
-//     std::cout << cor[i].first << ", " << cor[i].second << std::endl;
-// }
-
 Snake::Snake(int x, int y)
 {
     _head_coords.first = x;
     _head_coords.second = y;
-    _tail.push_back(std::make_pair(x, y + 1));
-    _tail.push_back(std::make_pair(x, y + 2));
-    _tail.push_back(std::make_pair(x, y + 3));
 
-    for (size_t i = 0; i < _tail.size(); i++)
-    {
-        std::cout << _tail[i].first << ", " << _tail[i].second << std::endl;
-    }
+    for (int i = 1; i <= 3; i++)
+        _tail.push_back(std::make_pair(x, y + i));
 }
