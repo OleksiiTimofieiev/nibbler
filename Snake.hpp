fc8164862aa9d184a7f9b1b753493ed24cfb1d16
contrbas@gmail.com <K4rmU78B>
2018-08-18 14:40:45 +0300

    ALL

--- Snake.hpp
+++ Snake.hpp
@@ -23,6 +23,7 @@ class Snake
 
     int _tail_len;
     Direction _dir;
+    Direction _prev_dir;
 
     public:
         Snake();
@@ -30,21 +31,24 @@ class Snake
         Snake &operator=(Snake const &rhs);
         Snake(Snake const &src);
         Snake(int x, int y);
-        std::pair<int, int>               getHeadCoords();
-        std::vector<std::pair<int, int> > getTailCoords();
-        Direction                         getDir();
-        void                              setHeadCoords(int x, int y);
-        void                              setDir(Direction dir);
-        void                              setTailLen();
-        void                              setTail();
-        int                               getTailLen();
-        void                              setTail(std::vector<std::pair<int, int> > tail);
+        std::pair<int, int> getHeadCoords();
+        std::vector<std::pair<int, int>> getTailCoords();
+        Direction getDir();
+        void setHeadCoords(int x, int y);
+        void setDir(Direction dir);
+        void setTailLen();
+        void setTail();
+        int getTailLen();
+        void setTail(std::vector<std::pair<int, int>> tail);
 
-        std::vector<std::pair<int, int> > getPrevTailCoords();
-        void                              setPrevTail(std::vector<std::pair<int, int>> tail);
+        std::vector<std::pair<int, int>> getPrevTailCoords();
+        void setPrevTail(std::vector<std::pair<int, int>> tail);
 
-        void                              setPrevHead(std::pair<int, int> _prev_head);
-        std::pair<int, int>               getPrevHeadCoords();
+        void setPrevHead(std::pair<int, int> _prev_head);
+        std::pair<int, int> getPrevHeadCoords();
+
+        Direction getPrevDir();
+        void setPrevDir(Direction dir);
 };
 
 #endif
