8a09aa9b0fe98c03d5192556af9eeb112e940997
contrbas@gmail.com <K4rmU78B>
2018-08-19 16:40:10 +0300

    ALL

--- Game.cpp
+++ Game.cpp
@@ -5,20 +5,22 @@ Game::~Game() {}
 
 void    Game::gameplay()
 {
-    IncGraphics inc;
+    //TODO: how the timing will work ?
+
+    IncGraphics *inc = new IncGraphics();
     Direction   dr = stop;
     clock_t t1;
     clock_t t2;
 
     t2 = 0;
-    inc.DrawMap(_border);
+    inc->DrawMap(_border);
     while(!_init.getGameOver())
     {
         t1 = clock() / (CLOCKS_PER_SEC / FPS);
         if (t1 > t2)
         {
-            inc.Draw(_snake, _fruits, _stat, _init);
-            dr = inc.CheckEvent(dr);
+            inc->Draw(_snake, _fruits, _stat, _init);
+            dr = inc->CheckEvent(dr);
             if (dr != stop)
                 _logic.logic(_init, _fruits, _snake, _stat, dr);
             t2 = clock() / (CLOCKS_PER_SEC / FPS);
