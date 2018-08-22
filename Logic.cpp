8561b27a2664fb20a4c8fc28dca235f477251d1f
contrbas@gmail.com <K4rmU78B>
2018-08-22 16:16:01 +0300

    ALL

--- Logic.cpp
+++ Logic.cpp
@@ -53,12 +53,21 @@ void    Logic::checkFruit(Fruit & fruit, Snake & snake, Score & score, int & fps
 
 void    Logic::checkCollision(Snake &snake, Init &init)
 {
-    if (snake.getHeadCoords().first > init.getWidth()   || snake.getHeadCoords().first < 0 ||
-        snake.getHeadCoords().second > init.getHeight() || snake.getHeadCoords().second < 0)
-    {
-        std::cout << "Border type collision." << std::endl;
-        init.setGameStatus();
-    }
+    if (snake.getHeadCoords().second > init.getHeight())
+        snake.setHeadCoords(snake.getHeadCoords().first, 0);
+    else if (snake.getHeadCoords().second == -1)
+        snake.setHeadCoords(snake.getHeadCoords().first, init.getHeight());
+    else if (snake.getHeadCoords().first > init.getWidth())
+        snake.setHeadCoords(0, snake.getHeadCoords().second);
+    else if (snake.getHeadCoords().first == -1)
+        snake.setHeadCoords(init.getWidth(), snake.getHeadCoords().second);
+
+    //         if (snake.getHeadCoords().first > init.getWidth() || snake.getHeadCoords().first < 0 ||
+    //             )
+    // {
+    //     std::cout << "Border type collision." << std::endl;
+    //     init.setGameStatus();
+    // }
 
     for (int i = 0; i < snake.getTailLen(); i++)
         if (snake.getTailCoords()[i] == snake.getHeadCoords())
